1cc9ef60fdf69955278637cfbfb6e72cd9459a9b
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-03-24 16:51:29 +0000

    added in rest of the templates

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function myButtonClicked () {
+  alert('Hello, World!')
 }
